1f1935be3d255cb747b506083ceebe44065213b9
yuyat-sushi <wahaha.yuy@gmail.com>
2025-04-05 00:37:24 +0900

    URL先のテキストを表示する仕組みを作った

--- Assets/ScryOne/Scripts/ScryButton.cs
+++ Assets/ScryOne/Scripts/ScryButton.cs
@@ -1,13 +1,37 @@
 ﻿
+using System.Text;
+using TMPro;
 using UdonSharp;
 using UnityEngine;
+using VRC.SDK3.StringLoading;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
 
 public class ScryButton : UdonSharpBehaviour
 {
-    void Start()
+    [SerializeField]
+    private TextMeshProUGUI TextUI;
+    [SerializeField]
+    private VRCUrl URL;
+
+    public override void Interact()
     {
-        
+        VRCStringDownloader.LoadUrl(URL, (IUdonEventReceiver)this);
+    }
+
+    public override void OnStringLoadSuccess(IVRCStringDownload result)
+    {
+        string resultAsUTF8 = result.Result;
+        byte[] resultAsBytes = result.ResultBytes;
+        string resultAsASCII = Encoding.ASCII.GetString(resultAsBytes);
+        Debug.Log($"UTF8: {resultAsUTF8}");
+        Debug.Log($"ASCII: {resultAsASCII}");
+        TextUI.text = resultAsUTF8;
+    }
+
+    public override void OnStringLoadError(IVRCStringDownload result)
+    {
+        Debug.LogError($"Error loading string: {result.ErrorCode} - {result.Error}");
     }
 }
